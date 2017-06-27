54f763c4ef8a2c70dccb803530a269fe3503005e
Arg0n1s <basti_seppl@yahoo.com>
2017-06-27 15:50:18 +0200

    aufgabe 2 läuft so weit

--- asgn3/code/mrf_posterior.jl
+++ asgn3/code/mrf_posterior.jl
@@ -12,11 +12,11 @@ function grad_mrf_denoise_nlposterior(x::Array{Float64,2}, y::Array{Float64,2}, 
 end
 
 function mrf_inpaint_nlposterior(x::Array{Float64,2}, m::BitArray{2}, sigma::Float64, alpha::Float64)
-
+    p = mrf_nlprior(x, sigma, alpha)
     return p::Float64;
 end
 
 function grad_mrf_inpaint_nlposterior(x::Array{Float64,2}, m::BitArray{2}, sigma::Float64, alpha::Float64)
-
-    return g::Float64;
+    g = grad_mrf_nlprior(x, sigma, alpha).*m
+    return g::Array{Float64, 2};
 end
