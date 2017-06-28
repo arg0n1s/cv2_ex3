27d8d6a1f15da17a5ce148d931a7465d78a87eb9
Arg0n1s <basti_seppl@yahoo.com>
2017-06-28 21:01:10 +0200

    ??

--- asgn3/code/problem3.jl
+++ asgn3/code/problem3.jl
@@ -13,37 +13,42 @@ function grad_loss(gt::Array{Float64,2}, X::Array{Float64,2})
 	return G::Array{Float64,2}
 end
 
+function grad_F(X::Array{Float64,2}, sigma::Float64, alpha::Float64)
+	alpha_helper = (x,s,a)->d_alpha_grad_studentt(x,s,a).*(studentt(x,s,a).^-1)-grad_studentt(x,s,a).*(studentt(x,s,a).^-2).*d_alpha_studentt(x,s,a)
+	sigma_helper = (x,s,a)->d_sigma_grad_studentt(x,s,a).*(studentt(x,s,a).^-1)-grad_studentt(x,s,a).*(studentt(x,s,a).^-2).*d_sigma_studentt(x,s,a)
+	dsigma = zeros(Float64, size(X))
+	dalpha = zeros(Float64, size(X))
+	for i in 1:size(X,1)
+			for j in 1:size(X,2)
+					da = 0
+					ds = 0
+					if j < size(X,2)
+							da += alpha_helper(X[i,j]-X[i,j+1],sigma, alpha)
+							ds += sigma_helper(X[i,j]-X[i,j+1],sigma, alpha)
+					end
+					if j > 1
+							da += -alpha_helper(X[i,j-1]-X[i,j],sigma, alpha)
+							ds += -sigma_helper(X[i,j-1]-X[i,j],sigma, alpha)
+					end
+					if i < size(X,1)
+							da += alpha_helper(X[i,j]-X[i+1,j],sigma, alpha)
+							ds += sigma_helper(X[i,j]-X[i+1,j],sigma, alpha)
+					end
+					if i > 1
+							da += -alpha_helper(X[i-1,j]-X[i,j],sigma, alpha)
+							ds += -sigma_helper(X[i-1,j]-X[i,j],sigma, alpha)
+					end
+					dalpha[i,j]=-da
+					dsigma[i,j]=-ds
+			end
+	end
+	return dsigma::Array{Float64,2}, dalpha::Array{Float64,2}
+end
+
 function prediction(X::Array{Float64,2}, Y::Array{Float64,2}, sigma_noise::Float64,
 					sigma::Float64, alpha::Float64)
-					F = X + grad_mrf_denoise_nlposterior(X, Y, sigma_noise, sigma, alpha)
-					alpha_helper = (x,a,s)->d_alpha_grad_studentt(x,s,a).*(studentt(x,s,a).^-1)-grad_studentt(x,s,a).*(studentt(x,s,a).^-2).*d_alpha_studentt(x,s,a)
-					sigma_helper = (x,a,s)->d_sigma_grad_studentt(x,s,a).*(studentt(x,s,a).^-1)-grad_studentt(x,s,a).*(studentt(x,s,a).^-2).*d_sigma_studentt(x,s,a)
-					dsigma = zeros(Float64, size(X))
-					dalpha = zeros(Float64, size(X))
-			    for i in 1:size(X,1)
-			        for j in 1:size(X,2)
-			            da = 0
-									ds = 0
-			            if j < size(X,2)
-											da += alpha_helper(X[i,j]-X[i,j+1],sigma, alpha)
-											ds += sigma_helper(X[i,j]-X[i,j+1],sigma, alpha)
-			            end
-			            if j > 1
-											da += alpha_helper(X[i,j-1]-X[i,j],sigma, alpha)
-											ds += sigma_helper(X[i,j-1]-X[i,j],sigma, alpha)
-			            end
-			            if i < size(X,1)
-											da += alpha_helper(X[i,j]-X[i+1,j],sigma, alpha)
-											ds += sigma_helper(X[i,j]-X[i+1,j],sigma, alpha)
-			            end
-			            if i > 1
-											da += alpha_helper(X[i-1,j]-X[i,j],sigma, alpha)
-											ds += sigma_helper(X[i-1,j]-X[i,j],sigma, alpha)
-			            end
-			            dalpha[i,j]=-da
-									dsigma[i,j]=-ds
-			        end
-			    end
+					F = X - grad_mrf_denoise_nlposterior(X, Y, sigma_noise, sigma, alpha)
+					dsigma, dalpha = grad_F(X, sigma, alpha)
 
 	return F::Array{Float64,2}, dsigma::Array{Float64,2}, dalpha::Array{Float64,2}
 end
@@ -53,8 +58,8 @@ function learning_objective(gt::Array{Float64,2}, X::Array{Float64,2}, Y::Array{
 	f, dalpha, dsigma = prediction(X, Y, sigma_noise, sigma, alpha)
 	J = -psnr(gt, f)
 	g = zeros(Float64, 2)
-	g[2] = sum(grad_loss(gt, f) .* -dalpha)
-	g[1] = sum(grad_loss(gt, f) .* -dsigma)
+	g[2] = sum(grad_loss(gt, f) .* dalpha)
+	g[1] = sum(grad_loss(gt, f) .* dsigma)
 
 	return J::Float64, g::Array{Float64,1}
 end
@@ -97,4 +102,8 @@ function problem3()
   imshow(img_noisy, "gray")
 	theta = find_optimal_params(img, img_noisy, img_noisy, sigma_noise, theta0)
 	display(theta)
+	#img_denoised = denoise(img_noisy, sigma_noise, theta[1], theta[2])
+  #PyPlot.figure()
+  #imshow(img_denoised, "gray")
+  #display(psnr(img, img_denoised))
 end
--- asgn3/code/studentt.jl
+++ asgn3/code/studentt.jl
@@ -9,17 +9,17 @@ function grad_studentt(d, sigma::Float64, alpha::Float64)
 end
 
 function d_alpha_studentt(d, sigma::Float64, alpha::Float64)
-  p = -alpha .* (1 .+ (1 / (2 * sigma^2)) .* (d.^2) ).^(-alpha-1)
+  p = -studentt(d, sigma, alpha) .* log(1 .+ ((d.^2) ./ (2*(sigma^2)))) 
   return p
 end
 
 function d_sigma_studentt(d, sigma::Float64, alpha::Float64)
-  p = -alpha .* ((d.^2) ./ (sigma^3)) .* (1 .+ ( (d.^2)./ (2* (sigma^2)) )).^(-alpha-1)
+  p = alpha .* ((d.^2) ./ (sigma^3)) .* (1 .+ ( (d.^2)./ (2* (sigma^2)) )).^(-alpha-1)
   return p
 end
 
 function d_alpha_grad_studentt(d, sigma::Float64, alpha::Float64)
-  p = d .* (1/ (sigma^2)) .* ((1 .+ ((d.^2) ./ (2*sigma^2))).^(-alpha-1)) .* (-1 + alpha * log(1 + (1/(2*sigma^2))))
+  p = d .* (1/ (sigma^2))  .* (-((1 .+ ((d.^2) ./ (2*sigma^2))).^(-alpha-1)) + ((1 .+ ((d.^2) ./ (2*sigma^2))).^(-alpha-1)).*alpha.*log(1 .+ ((d.^2)./(2*sigma^2))))
   return p
 end
 
